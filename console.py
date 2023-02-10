d2d5821c5b593bdec281cdd594e9ff78e874a129
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-02-10 23:44:32 +0200

    console

--- console.py
+++ console.py
@@ -45,8 +45,8 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] not in HBNBCommand.class_list:
             print("* class doesn't exist *")
             elif len(args) < 6:
-    print("* instance id missing *")
-            else:
+            print("* instance id missing *")
+          else:
             key = args[0] + "." + args[1]
             if key in models.storage.all():
                 print(models.storage.all()[key])
